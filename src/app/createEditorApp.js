f41a4120b9fa2c7474c64e41e008194634b7fd37
WalterDevIn <144739473+WalterDevIn@users.noreply.github.com>
2026-06-26 15:14:32 -0300

    Add editor app composition root

---
+++ src/app/createEditorApp.js
@@ -0,0 +1,33 @@
+import { createEditorController } from "../editor/editorController.js";
+import { createEditorState } from "../editor/editorState.js";
+import { installEditorEvents } from "../editor/installEditorEvents.js";
+import { renderEditor, renderEditorError } from "../view/renderEditor.js";
+
+export function createEditorApp({ rootElement }) {
+  const editorState = createEditorState();
+  let controller = null;
+
+  function render() {
+    rootElement.innerHTML = "";
+    rootElement.appendChild(renderEditor({ editorState, controller }));
+  }
+
+  controller = createEditorController({ editorState, render });
+
+  function start() {
+    try {
+      installEditorEvents({ editorState, controller });
+      render();
+    } catch (error) {
+      console.error(error);
+      rootElement.innerHTML = "";
+      rootElement.appendChild(renderEditorError(error));
+    }
+  }
+
+  return {
+    editorState,
+    controller,
+    start,
+  };
+}
