56386ba3209b93270d34302b7bb400a7c5d046a6
WalterDevIn <144739473+WalterDevIn@users.noreply.github.com>
2026-06-26 15:04:20 -0300

    Add editor constants module

---
+++ src/core/constants.js
@@ -0,0 +1,23 @@
+export const PAGE_WIDTH_MM = 105;
+export const PAGE_HEIGHT_MM = 147.5;
+export const GRID_MM = 5;
+
+export const FONTS = [
+  "Arial",
+  "Georgia",
+  "Times New Roman",
+  "Courier New",
+  "Verdana",
+  "Trebuchet MS",
+  "Impact",
+  "Comic Sans MS",
+];
+
+export const DEFAULT_BLOCK = {
+  text: "Texto",
+  x: 10,
+  y: 10,
+  width: 35,
+  height: 15,
+  fontFamily: "Arial",
+};
