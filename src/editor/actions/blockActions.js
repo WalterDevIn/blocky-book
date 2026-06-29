657f951f5d1cee3eab2f2a74373db444ce8f5c1d
WalterDevIn <144739473+WalterDevIn@users.noreply.github.com>
2026-06-29 14:22:37 -0300

    Add pending block placement flow

--- src/editor/actions/blockActions.js
+++ src/editor/actions/blockActions.js
@@ -1,4 +1,5 @@
 import { BLOCK_TYPES } from "../../blocks/blockTypes.js";
+import { getBlockDefinition } from "../../blocks/blockRegistry.js";
 import {
   addBlockToPage,
   deleteBlocks,
@@ -10,6 +11,8 @@ import {
   updateBlocksProps as updateDocumentBlocksProps,
 } from "../../document/documentCommands.js";
 import { findBlockById, getFirstPage } from "../../document/documentQueries.js";
+import { PAGE_SPEC } from "../../document/printSpec.js";
+import { clamp, snapMm } from "../../shared/geometry.js";
 import { createSelection, getSelectedBlockIds } from "../selectionHelpers.js";
 
 const DROP_ANIMATION_MS = 180;
@@ -31,9 +34,19 @@ export function createBlockActions({ editorState, render, mutateDocument }) {
   }
 
   function addBlock(type) {
-    const page = getFirstPage(editorState.document);
-    const block = mutateDocument((documentModel) => addBlockToPage(documentModel, page.id, type));
-    editorState.selection = createSelection([block.id], page.id);
+    editorState.activeTool = type;
+    editorState.interaction.contextMenu = null;
+    editorState.interaction.editingBlockId = null;
+    render();
+  }
+
+  function createBlockAtPagePoint(type, pageId, point) {
+    const definition = getBlockDefinition(type);
+    const frame = getFrameUnderPointer(definition.defaultFrame, point, editorState.document.pageSpec ?? PAGE_SPEC);
+    const block = mutateDocument((documentModel) => addBlockToPage(documentModel, pageId, type, { frame }));
+
+    editorState.selection = createSelection([block.id], pageId);
+    editorState.activeTool = "select";
     editorState.interaction.contextMenu = null;
     editorState.interaction.editingBlockId = null;
     render();
@@ -44,6 +57,13 @@ export function createBlockActions({ editorState, render, mutateDocument }) {
 
     addTextBlock() {
       addBlock(BLOCK_TYPES.text);
+    },
+
+    createBlockAtPagePoint,
+
+    cancelPendingBlockPlacement() {
+      editorState.activeTool = "select";
+      render();
     },
 
     deleteSelectedBlock() {
@@ -154,5 +174,16 @@ export function createBlockActions({ editorState, render, mutateDocument }) {
       mutateDocument((documentModel) => updateDocumentBlocksProps(documentModel, blockIds, props));
       render();
     },
+  };
+}
+
+function getFrameUnderPointer(defaultFrame, point, pageSpec) {
+  const x = snapMm(point.x - defaultFrame.width / 2, pageSpec.gridMm);
+  const y = snapMm(point.y - defaultFrame.height / 2, pageSpec.gridMm);
+
+  return {
+    ...defaultFrame,
+    x: clamp(x, 0, pageSpec.widthMm - defaultFrame.width),
+    y: clamp(y, 0, pageSpec.heightMm - defaultFrame.height),
   };
 }
